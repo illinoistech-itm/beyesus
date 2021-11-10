f1f6f22d5650ab3d59257376d2abcb3980f3708c
birukye <birukye@gmail.com>
2021-11-09 18:40:40 -0600

    edited riemannb

--- itmo-553/packer-example-code/scripts/post_install_vagrant-centos-8-riemannb.sh
+++ itmo-553/packer-example-code/scripts/post_install_vagrant-centos-8-riemannb.sh
@@ -10,7 +10,7 @@ set -v
 # https://github.com/mitchellh/vagrant/issues/1482
 #echo "Defaults requiretty" | sudo tee -a /etc/sudoers.d/init-users
 # Need to add this first as wget not part of the base package...
-sudo yum install -y wget
+sudo yum install -y wget git
 #################################################################################################################
 # code needed to allow for vagrant to function seamlessly
 #################################################################################################################
@@ -31,25 +31,43 @@ echo "All Done!"
 #########################
 # Add customization here
 #########################
-sudo yum install -y java-1.8.0-openjdk ruby ruby-devel
-wget https://github.com/riemann/riemann/releases/download/0.3.6/riemann-0.3.6-1.noarch-EL8.rpm
-sudo rpm -Uvh riemann-0.3.6-1.noarch-EL8.rpm
-sudo gem install riemann-client riemann-tools riemann-dash
-sudo systemctl enable riemann
-sudo systemctl start riemann
-sudo yum install -y kernel-devel-`uname -r` gcc binutils make perl bzip2 vim git rsync
-sudo yum install -y epel-release
-sudo yum install -y python3-pip python3-setuptools python3-devel gcc libffi-devel cairo-devel libtool
-sudo yum install -y python3-six  python3-urllib3
-#sudo yum install -y graphite-carbon python3-whisper
-#sudo yum install graphite-api gunicorn
-#sudo yum install -y adduser libfontconfig1
-sudo python3 -m pip install carbon
-sudo yum install -y urw-fonts
-wget https://dl.grafana.com/oss/release/grafana-7.3.6-1.x86_64.rpm
-sudo rpm -iv ./grafana-7.3.6-1.x86_64.rpm
-git clone git@github.com:illinoistech-itm/beyesus.git
-cp -v sample-student/itmo-453/week-11/riemann/riemannb/riemann.config /etc/riemann/riemann.config
-sudo systemctl enable riemann
-sudo systemctl start riemann
+
+sudo yum install -y kernel-devel-`uname -r` gcc binutils make perl bzip2
+
 echo "All Done!"
+
+cat << EOT >> /etc/hosts
+# Nodes
+192.168.33.100  riemanna riemanna.example.com
+192.168.33.101  riemannb riemannb.example.com
+192.168.33.102  riemannmc riemannmc.example.com
+192.168.33.200  graphitea graphitea.example.com
+192.168.33.201  graphiteb graphiteb.example.com
+192.168.33.202  graphitemc graphitemc.example.com
+EOT
+
+## Command to change hostname
+sudo hostnamectl set-hostname riemannb
+
+# Install software
+# 1 we will need openjdk-8-jre (java runtime) and ruby runtimes
+# Examples:
+sudo yum install -y java-1.8.0-openjdk ruby ruby-devel
+
+# 2 we will need the rpm deb packages from riemann.io
+# Examples
+wget https://github.com/riemann/riemann/releases/download/0.3.6/riemann-0.3.6-1.noarch-EL8.rpm
+sudo rpm -i riemann-0.3.6-1.noarch-EL8.rpm
+# 3 we will need some ruby gems 
+sudo gem install riemann-client riemann-tools riemann-dash 
+# 4 We need to ensure the services are enabled and start succesfully
+sudo systemctl enable riemann
+sudo systemctl start riemann
+
+git clone git@github.com:illinoistech-itm/beyesus.git
+cp -v beyesus/itmo-553/week-07/riemann/riemannb/riemann.config /etc/riemann/riemann.conf
+
+sudo systemctl stop riemann
+sudo systemctl start riemann
+
+
